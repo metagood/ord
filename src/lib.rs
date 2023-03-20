6f03ec1ac76b39c5664a892d3af944e20b07e7b3
ericatallah <eric.atallah@gmail.com>
2023-03-19 21:30:56 -0700

    copy Felipe PR 9 to new branch off master
    Co-authored-by: felipelincoln <mail.felipelincoln@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -54,8 +54,8 @@ use {
     env,
     ffi::OsString,
     fmt::{self, Display, Formatter},
-    fs::{self, File},
-    io,
+    fs::{self, File, OpenOptions},
+    io::{self, Write},
     net::{TcpListener, ToSocketAddrs},
     ops::{Add, AddAssign, Sub},
     path::{Path, PathBuf},
@@ -144,7 +144,18 @@ fn timestamp(seconds: u32) -> DateTime<Utc> {
 const INTERRUPT_LIMIT: u64 = 5;
 
 pub fn main() {
-  env_logger::init();
+  let inscription_satpoint_logs_file = OpenOptions::new()
+    .write(true)
+    .append(true)
+    .create(true)
+    .open("inscription_satpoint.txt")
+    .unwrap();
+
+  env_logger::builder()
+    .filter(Some("new_inscription_satpoint"), log::LevelFilter::Info)
+    .target(env_logger::Target::Pipe(Box::new(inscription_satpoint_logs_file)))
+    .format(|buf, record| writeln!(buf, "{}", record.args()))
+    .init();
 
   ctrlc::set_handler(move || {
     LISTENERS
