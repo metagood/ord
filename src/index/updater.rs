a7d403fdaa2398cef64963ff2c598463ed1376ed
ericatallah <eric.atallah@gmail.com>
2023-03-24 15:15:56 -0700

    insignificant change

--- src/index/updater.rs
+++ src/index/updater.rs
@@ -102,7 +102,6 @@ impl Updater {
 
     let mut uncommitted = 0;
     let mut value_cache = HashMap::new();
-
     let mut prev_block_hash: Option<BlockHash> = if let Some(prev_height) = self.height.checked_sub(1) {
       Some(index.client.get_block_hash(prev_height).unwrap())
     } else {
