052bb96325375e0fc878c5538610e13d8a9e1980
Felipe Lincoln <mail.felipelincoln@gmail.com>
2023-03-24 21:32:04 -0300

    change logic to use inscription id

--- src/subcommand.rs
+++ src/subcommand.rs
@@ -46,7 +46,7 @@ pub(crate) enum Subcommand {
   Traits(traits::Traits),
   #[clap(subcommand, about = "Wallet commands")]
   Wallet(wallet::Wallet),
-  #[clap(about = "Extract inscription data from witness")]
+  #[clap(subcommand, about = "Extract inscription data from witness")]
   Witness(witness::Witness),
 }
 
