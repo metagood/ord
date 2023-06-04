65e4acd81a118f4abae68b428308f75099474c1a
ericatallah <eric.atallah@gmail.com>
2023-06-04 15:00:10 -0700

    add logfile to lib, log block data, fix csp

--- src/subcommand/server.rs
+++ src/subcommand/server.rs
@@ -745,7 +745,7 @@ impl Server {
     );
     headers.insert(
       header::CONTENT_SECURITY_POLICY,
-      HeaderValue::from_static("default-src 'unsafe-eval' 'unsafe-inline' data:"),
+      HeaderValue::from_static("default-src 'self' 'unsafe-eval' 'unsafe-inline' data:"),
     );
     headers.insert(
       header::CACHE_CONTROL,
@@ -2202,7 +2202,7 @@ mod tests {
     server.assert_response_csp(
       format!("/preview/{}", InscriptionId::from(txid)),
       StatusCode::OK,
-      "default-src 'unsafe-eval' 'unsafe-inline' data:",
+      "default-src 'self' 'unsafe-eval' 'unsafe-inline' data:",
       "hello",
     );
   }
